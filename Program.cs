c23e676f203a21a44dececcd7387bf7983ea11b1
J. Rivaldo <j.rivaldo.f.barbosa@outlook.com>
2024-09-16 16:39:29 -0300

    Desafio concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("----- Nokia -----");
+Nokia nokia = new Nokia("01234","Modelo 09", "333333", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Kanji Study");
+
+Console.WriteLine("----------------------");
+
+Console.WriteLine("----- IPhone -----");
+Iphone iphone = new Iphone("56789","Modelo 18", "666666", 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
